0a516d6dff4a304f6bff6346480b81e4379cbb4b
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-08-15 16:01:38 -0300

    implementa plugin de detecção de spam. Ref: #84

---
+++ Plugin.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace SpamDetector;
+use Mustache;
+use MapasCulturais\App;
+use MapasCulturais\i;
+use MapasCulturais\Entities\Notification;
+
+class Plugin extends \MapasCulturais\Plugin
+{
+    public function __construct($config = [])
+    {
+        $config += [
+            'terms' => env('SPAM_DETECTOR_TERMS', ['citotec', 'minecraft']),
+            'entities' => env('SPAM_DETECTOR_ENTITIES', ['Agent', 'Opportunity', 'Project', 'Space', 'Event']),
+            'fields' => env('SPAM_DETECTOR_FIELDS', ['name', 'shortDescription', 'longDescription']),
+        ];
+
+        parent::__construct($config);
+    }
+
+    public function _init()
+    {
+        $app = App::i();
+        $plugin = $this;
+        
+        $hooks = implode('|', $plugin->config['entities']);
+
+        // add hooks
+        $app->hook("entity(<<{$hooks}>>).<<update|create>>:after", function() use ($plugin, $app) {
+            $admins = ['saasSuperAdmin','admin'];
+            $user_ids = [];
+            $roles = $app->repo('Role')->findAll();
+
+            if ($roles) {
+                foreach ($roles as $role) {
+                    if (in_array($role->name, $admins)) {
+                        $user_ids[] = $role->userId;
+                    }
+                }
+            }
+            
+            $terms = $plugin->config['terms'];
+            $fields = $plugin->config['fields'];
+            
+            $spamDetector = [];
+            
+            foreach ($fields as $field) {
+                $foundTerms = [];
+                if ($value = $this->$field) {
+                    $lowercaseValue = mb_strtolower($value);
+                    foreach ($terms as $term) {
+                        if (strpos($lowercaseValue, mb_strtolower($term)) !== false && !in_array($term, $foundTerms)) {
+                            $foundTerms[] = $term;
+                        }
+                    }
+                    
+                    if (!empty($foundTerms)) {
+                        $spamDetector[] = [
+                            'terms' => $foundTerms,
+                            'field' => $field,
+                        ];
+                    }
+                }
+            }
+
+            if (!empty($spamDetector)) {
+                foreach ($user_ids as $id) {
+                    $agents = $app->repo('Agent')->findBy(['userId' => $id]);
+                    foreach ($agents as $agent) {
+                        $plugin->createNotification($agent, "Possível spam detectado. Verifique seu email.", $this, $spamDetector);
+                    } 
+                }
+                
+            }
+        });
+    }
+
+    public function register()
+    {
+    }
+    
+    public function createNotification($agent, $message, $entity, $spamDetections)
+    {
+        $app = App::i();
+        $app->disableAccessControl();
+        
+        $notification = new Notification;
+        $notification->user = $agent->user;
+        $notification->message = $message;
+        $notification->save(true);
+        
+        $filename = $app->view->resolveFilename("views/emails", "email-spam.html");       
+        $template = file_get_contents($filename);
+        
+        $fieldTranslations = [
+            "name" => i::__("Nome"),
+            "shortDescription" => i::__("Descrição Curta"),
+            "longDescription" => i::__("Descrição Longa"),
+        ];
+
+        $detectedDetails = [];
+        foreach ($spamDetections as $detection) {
+            $translatedField = isset($fieldTranslations[$detection['field']]) ? $fieldTranslations[$detection['field']] : $detection['field'];
+            $detectedDetails[] = "Campo: $translatedField, Termos: " . implode(', ', $detection['terms']) . '<br>';
+        }
+        
+        $params = [
+            "siteName" => $app->siteName,
+            "nome" => $entity->name,
+            "id" => $entity->id,
+            "url" => $entity->singleUrl,
+            "baseUrl" => $app->getBaseUrl(),
+            "detectedDetails" => implode("\n", $detectedDetails), 
+        ];
+
+        $mustache = new \Mustache_Engine();
+        $content = $mustache->render($template,$params);
+        
+        if ($agent->emailPrivado) {
+            $app->createAndSendMailMessage([
+                'from' => $app->config['mailer.from'],
+                'to' => $agent->emailPrivado,
+                'subject' => 'Notificação de spam',
+                'body' => $content,
+            ]);
+        }
+
+        $app->enableAccessControl();
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # plugin-SpamDetector
+> Plugin de detecção de spam para uso na plataforma Mapas Culturais
+
+## Efeitos do uso do plugin
+
+- O plugin implementa na plataforma um sistema de detecção automática de spam, identificando termos suspeitos em entidades como Agente, Oportunidade, Projeto, Espaço e Evento, ajudando a manter o conteúdo da plataforma seguro e livre de spam.
+
+## Requisitos Mínimos
+
+- Mapas Culturais v7.0.0^
+
+## Configuração básica
+
+### Configurações necessárias para uso no ambiente de desenvolvimento ou produção
+
+- No arquivo `docker/common/config.d/plugins.php`, adicione a linha `'SpamDetector'` para ativar o plugin:
+
+    ```php
+    <?php
+
+    return [
+        'plugins' => [
+            'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
+            'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+            "SpamDetector",
+        ]
+    ];
+    ```
+
+## Como funciona
+
+- O plugin monitora atualizações e criações de entidades específicas (Agente, Oportunidade, Projeto, Espaço, Evento) e verifica se os campos configurados (por padrão, `name`, `shortDescription`, `longDescription`) contêm termos suspeitos predefinidos, como "citotec" e "minecraft".
+- Se termos suspeitos forem encontrados, o plugin notifica automaticamente os administradores (super admins e admins) via e-mail, listando os termos detectados e os campos onde foram encontrados.
+
+## Personalização
+
+- As configurações do plugin permitem personalizar os termos a serem detectados (`terms`), as entidades monitoradas (`entities`), e os campos onde a detecção deve ocorrer (`fields`). Essas configurações podem ser definidas dentro de uma chave chamada `config` no arquivo `docker/common/config.d/plugins.php`, como mostrado abaixo:
+
+    ```php
+    <?php
+
+    return [
+        'plugins' => [
+            'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
+            'SamplePlugin' => ['namespace' => 'SamplePlugin'],
+            "SpamDetector" => [
+                "namespace" => "SpamDetector",
+                "config" => [
+                    // suas configurações personalizadas abaixo, por exemplo:
+                    "terms" => ['citotec', 'minecraft', 'venda', 'cytotec'],
+                ]
+            ]
+        ]
+    ];
+    ```
+
+- **IMPORTANTE:** Ao adicionar configurações personalizadas na chave `config`, o que for adicionado irá **sobrescrever** a configuração padrão. Certifique-se de incluir todos os parâmetros necessários para evitar comportamentos indesejados.
+
+## Notificações
+
+- Quando um possível spam é detectado, uma notificação é enviada aos administradores cadastrados, e um e-mail é gerado usando um template Mustache (`email-spam.html`). O e-mail contém detalhes sobre os termos e os campos onde foram encontrados, juntamente com um link para a entidade suspeita na plataforma.
+
